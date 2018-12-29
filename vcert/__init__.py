9145ee8895a7a951feb65af56bdd330350ea6308
Denis Subbotin <denis@subbot.in>
2018-12-29 11:34:35 +0000

    fix some TODOs

--- vcert/__init__.py
+++ vcert/__init__.py
@@ -4,7 +4,7 @@ from .connection_fake import FakeConnection
 from .common import CertificateRequest, CommonConnection, KeyTypes
 
 
-def Connection(url=None, token=None, user=None, password=None):
+def Connection(url=None, token=None, user=None, password=None, ignore_ssl_errors=False):
     """
     Return connection based on credentials list.
     TPP required url, user, password
@@ -14,12 +14,13 @@ def Connection(url=None, token=None, user=None, password=None):
     :param str token: cloud token
     :param str user: tpp user
     :param str password: tpp password
+    :param bool ignore_ssl_errors: Option for work with untrusted tpp https certificate (only for tpp).
     :rtype CommonConnection:
     """
     if not (token or url or user or password):
         return FakeConnection()
     if url and user and password:
-        return TPPConnection(user=user, password=password, url=url)
+        return TPPConnection(user=user, password=password, url=url, ignore_ssl_errors=ignore_ssl_errors)
     if token:
         return CloudConnection(token=token, url=None)
     raise Exception("Bad credentials list")
