b9f647e81690d7f4726c17e0520cc062eb3e0eb5
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2019-01-23 15:29:34 +0300

    fixing CN is binary type

--- vcert/common.py
+++ vcert/common.py
@@ -296,7 +296,10 @@ class CertificateRequest:
         if key == "key_password":
             if isinstance(value, string_types):
                 value = value.encode()
-        if key == "private_key":
+        elif key == "common_name":
+            if isinstance(value, binary_type):
+                value = value.decode()
+        elif key == "private_key":
             if isinstance(value, string_types):
                 value = serialization.load_pem_private_key(value.encode(),
                                                            password=self.key_password, backend=default_backend())
