9f57be2ac5de6ed6862f5bfcc35f577ab80ad106
Denis Subbotin <denis@subbot.in>
2019-01-30 15:56:15 +0000

    Push version to 0.5.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='vcert',
-      version='0.4.2',
+      version='0.5.0',
       url="https://github.com/Venafi/vcert-python",
       packages=['vcert'],
       install_requires=['requests>=2.20.0', 'python-dateutil>=2.6.1', 'certvalidator',
