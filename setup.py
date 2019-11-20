7764439e0b26a87b8ea3d5b450b130c0f50c03a0
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2019-11-20 17:35:44 +0300

    Updating version to 0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='vcert',
-      version='0.6.9',
+      version='0.7.0',
       url="https://github.com/Venafi/vcert-python",
       packages=['vcert'],
       install_requires=['requests', 'python-dateutil>=2.6.1', 'certvalidator', 'six',
