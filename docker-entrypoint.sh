ee17ab833eeeb50578ccc637568ea16d70c0f80e
Denis Subbotin <denis@subbot.in>
2020-04-13 15:55:57 +0300

    remove python version
    Signed-off-by: Denis Subbotin <denis@subbot.in>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ set -o pipefail
 
 bandit -r vcert/
 safety check
-py.test -v --junit-xml=junit.xml --junit-prefix=`python3 -V | tr ' ' '_'`
+py.test -v --junit-xml=junit.xml --junit-prefix=`python -V | tr ' ' '_'`
