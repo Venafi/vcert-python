649a970d8078b54fa6d9ada034e75a798497c30d
Ryan Treat <tr1ck3r@users.noreply.github.com>
2021-05-04 17:07:15 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,8 @@ set -o nounset
 set -o pipefail
 
 bandit -r vcert/
-safety check
+
+# ID 40291 is pip, ignore so we can still test python 2.7
+safety check -i 40291
+
 py.test -v --junit-xml=junit.xml --junit-prefix=`python -V | tr ' ' '_'`
