6b276e81d3b5da45cfc5fe60fe16bc315d216e10
marcos-albornoz <74274513+marcos-albornoz@users.noreply.github.com>
2021-11-30 14:06:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(base_dir, "README.md")) as f:
 # README = (HERE / "README.md").read_text()
 
 setup(name='vcert',
-      version='0.13.0',
+      version='0.13.1',
       url="https://github.com/Venafi/vcert-python",
       packages=['vcert', 'vcert.parser', 'vcert.policy'],
       install_requires=['requests<=2.25.1', 'python-dateutil<=2.8.1', 'certvalidator<=0.11.1', 'six<=1.15.0',
