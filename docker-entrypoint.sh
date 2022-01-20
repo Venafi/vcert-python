5d3783b5db3e4c7cdc10d5d5140b6488d2d5ce6c
Russel Vela <russel.vela@venafi.com>
2022-01-19 23:46:34 -0600

    -Added vcert.parser and vcert.policy modules to coverage report

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,4 @@ bandit -r vcert/
 # ID 40291 is pip, ignore so we can still test python 2.7
 safety check -i 40291
 
-pytest -v --junit-xml=junit.xml --junit-prefix=`python -V | tr ' ' '_'` --cov=vcert
+pytest -v --junit-xml=junit.xml --junit-prefix=`python -V | tr ' ' '_'` --cov=vcert --cov=vcert.parser --cov=vcert.policy
